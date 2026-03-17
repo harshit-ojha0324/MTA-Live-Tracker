d6253c7e9d5ffbc597dc7abd448eb2f09a9ab8f6
Harshit Ojha <harshitojha0324@gmail.com>
2026-03-17 17:47:39 -0400

    add docker support for local use

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: process.env.BACKEND_URL || 'http://localhost:5001',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5001',
+        target: process.env.BACKEND_URL || 'http://localhost:5001',
         changeOrigin: true,
         ws: true,
       },
